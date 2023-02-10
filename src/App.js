ba1d5548e44b88e3f8198ee1cca996803d3d85dd
jjolraman <jjolraman@gmail.com>
2023-02-10 21:08:25 +0900

    저장용 commit

--- src/App.js
+++ src/App.js
@@ -3,18 +3,40 @@ import { useState } from "react";
 
 function App() {
   let post = "강남 우동 맛집";
-  let [글제목, a] = useState([
+  let [글제목, 글제목변경] = useState([
     "남자 코트 추천",
     "강남 우동 맛집",
     "파이썬독학",
   ]);
   let [따봉, 따봉변경] = useState(0);
+  let [modal, setModal] = useState(false);
 
   return (
     <div className="App">
       <div className="black-nav">
         <h4>ReactBlog</h4>
       </div>
+
+      <button
+        onClick={() => {
+          let copy = [...글제목];
+          copy.sort();
+          글제목변경(copy);
+        }}
+      >
+        가나다순정렬
+      </button>
+
+      <button
+        onClick={() => {
+          let copy = [...글제목];
+          copy[0] = "여자코트 추천";
+          글제목변경(copy);
+        }}
+      >
+        글수정
+      </button>
+
       <div className="list">
         <h4>
           {글제목[0]}{" "}
@@ -34,10 +56,27 @@ function App() {
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
-        <h4>{글제목[2]}</h4>
+        <h4
+          onClick={() => {
+            setModal(!modal);
+          }}
+        >
+          {글제목[2]}
+        </h4>
         <p>2월 17일 발행</p>
       </div>
-      <h4>{post}</h4>
+
+      {modal == true ? <Modal /> : null}
+    </div>
+  );
+}
+
+function Modal() {
+  return (
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
     </div>
   );
 }
